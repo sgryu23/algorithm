23cfdbc6f8d5d53420f008e2874750cc7e68eea4
Seunggwang Ryu <139521783+sgryu23@users.noreply.github.com>
2024-01-17 23:16:33 +0900

    [Gold V] Title: LCS, Time: 528 ms, Memory: 55708 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,14 +1,15 @@
-str1 = input()
-str2 = input()
-l1 = len(str1)
-l2 = len(str2)
-dp = [[0] * (l2 + 1) for _ in range(l1 + 1)]
+import sys
+input = sys.stdin.readline
 
-for i in range(1, l1 + 1):
-    for j in range(1, l2 + 1):
-        if str1[i-1] == str2[j-1]:  # 만약 두 문자열이 같다면
-            dp[i][j] = dp[i-1][j-1] + 1
-        else:    # 두 문자열이 같지 않으면
-            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+string1 = list(input().strip())
+string2 = list(input().strip())
 
-print(dp[l1][l2])
+dp = [[0] * (len(string1) + 1) for _ in range(len(string2) + 1)]
+
+for row in range(1, len(string2) + 1):
+    for col in range(1, len(string1) + 1):
+        if string1[col - 1] == string2[row - 1]:
+            dp[row][col] = dp[row - 1][col - 1] + 1
+        else:
+            dp[row][col] = max(dp[row - 1][col], dp[row][col - 1])
+print(dp[-1][-1])
