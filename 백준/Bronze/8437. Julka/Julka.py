5617b2d772f0ec68504aacd4f196a91088e4bb60
Seunggwang Ryu <139521783+sgryu23@users.noreply.github.com>
2024-04-27 21:11:30 +0900

    [Bronze V] Title: Julka, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/8437. Julka/Julka.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+apples = int(input())
+subtraction = int(input())
+
+Klauida = (apples + subtraction) // 2
+Natalii = (apples - subtraction) // 2
+
+print(Klauida)
+print(Natalii)
