44e3c19c9d9736fa3ade0212e24f75d2216e8f0b
Seunggwang Ryu <139521783+sgryu23@users.noreply.github.com>
2024-05-31 23:13:57 +0900

    [Silver II] Title: 베르트랑 공준, Time: 1336 ms, Memory: 219912 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,28 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-
-def prime_number(number):
-    if number < 2:
-        return False
-    elif number == 2:
-        return True
-    elif number % 2 == 0:
-        return False
-    else:
-        last = round(number ** 0.5) + 1
-        for i in range(3, last, 2):
-            if number % i == 0:
-                return False
-    return True
-
-
 while True:
     x = int(input())
     if x == 0:
         break
-    answer = 0
-    for j in range(x + 1, x * 2 + 1):
-        if prime_number(j):
-            answer += 1
+
+    is_prime_number = [0] * 2 + [1] * 123456 * 2
+
+    # 에라토스테네스의 체 방법
+    for i in range(2, 123456 * 2 + 1):
+        # 소수가 아닌 것들은 지워준다.
+        if is_prime_number[i]:
+            for j in range(i * 2, 123456 * 2 + 1, i):
+                is_prime_number[j] = 0
+
+    answer = sum(is_prime_number[x + 1: x * 2 + 1])
     print(answer)
