0571b4499dd066d413d60120bfd372186d182457
Seunggwang Ryu <139521783+sgryu23@users.noreply.github.com>
2024-06-20 23:05:55 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+answer = N
+
+for i in range(N):
+    word = input().rstrip()
+    for j in range(len(word) - 1):
+        if word[j] == word[j + 1]:
+            pass
+        elif word[j] in word[j + 1:]:
+            answer -=1
+            break
+
+print(answer)
